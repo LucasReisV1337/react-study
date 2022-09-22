e8bda7289ad3879108fad1067f1a453411e512b9
lucas <lucasreisvasconcelos@icloud.com>
2022-09-21 21:16:12 -0300

    Fim aula #16

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import Condicional from './components/Condicional';
-
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
+  
+  
   return (
-      <div className = "App" >
-      <h1>Renderização Condicional</h1>
-      <Condicional/>
-      
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route path='/empresa' element={<Empresa />}></Route>
+        <Route path='/contato' element={<Contato/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/components/OutraLista.js
@@ -0,0 +1,14 @@
+function OutraLista({ itens }) {
+  return (
+    <>
+      <h3>Lista de coisas boas:</h3>
+      {itens.length > 0 ? (
+        itens.map((item, index) => <p key={index}>{item}</p>)
+      ) : (
+        <p>Nao ha itens cadastrados</p>
+      )}
+    </>
+  );
+}
+
+export default OutraLista;
